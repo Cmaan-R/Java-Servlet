c04cd65046e3fff7690a5045707cd4a449f62111
Windows 10 <Windows 10@DESKTOP-ANGJ5V6>
2022-10-30 17:51:20 -0300

    Update

--- gerenciador/src/main/java/br/com/alura/gerenciador/acao/Login.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/acao/Login.java
@@ -6,18 +6,30 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import br.com.alura.gerenciador.modelo.Banco;
+import br.com.alura.gerenciador.modelo.Usuario;
+
 public class Login implements Acao {
 
 	@Override
 	public String executa(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
+
 		String login = request.getParameter("login");
 		String senha = request.getParameter("senha");
-		
+
 		System.out.println("Logando " + login);
-		
-		return "redirect:entrada?acao=listaEmpresas";
+
+		Banco banco = new Banco();
+		Usuario usuario = banco.existeUsuario(login, senha);
+
+		if (usuario != null) {
+			System.out.println("Usuario existe");
+			return "redirect:entrada?acao=listaEmpresas";
+		} else {
+			return "redirect:entrada?acao=loginForm";
+		}
+
 	}
 
 }
