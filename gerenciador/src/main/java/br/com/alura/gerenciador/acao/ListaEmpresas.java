179381b82ff9784fc83289409e6e022a02523854
Windows 10 <Windows 10@DESKTOP-ANGJ5V6>
2022-10-31 09:42:14 -0300

    Update

--- gerenciador/src/main/java/br/com/alura/gerenciador/acao/ListaEmpresas.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/acao/ListaEmpresas.java
@@ -16,11 +16,6 @@ public class ListaEmpresas implements Acao {
 	public String executa(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		HttpSession sessao = request.getSession();
-		if (sessao.getAttribute("usuarioLogado") == null) {
-			return "redirect:entrada?acao=loginForm";
-		}
-
 		System.out.println("Listando Empresas");
 
 		Banco banco = new Banco();
--- gerenciador/src/main/java/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,7 +12,7 @@ import javax.servlet.http.HttpSession;
 
 import br.com.alura.gerenciador.acao.Acao;
 
-//@WebServlet("/entrada")
+@WebServlet("/entrada")
 public class UnicaEntradaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
